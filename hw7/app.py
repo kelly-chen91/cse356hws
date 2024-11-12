75924cd11248be43dae8df43b88e7246c5ad6bd9
Kelly Chen <kellychen966@gmail.com>
2024-11-11 21:02:39 -0500

    oops

--- hw7/app.py
+++ hw7/app.py
@@ -19,21 +19,21 @@ def hw6():
     temp = quote(player)
     result = cache.get(temp)
     app.logger.info(f'Cached Result: {result}')
-    # if not result: 
-    # create a connection cursor
-    with engine.connect() as connection:
-        result = connection.execute(text("""
-                select A.Player as p1,B.Player as p2, C.Player as p3,D.Player as p4 
-                from assists A, assists B, assists C, assists D 
-                where A.POS=B.POS and B.POS=C.POS and C.POS=D.POS and A.Club<>B.Club and A.club<>C.Club and A.Club<>C.Club and A.Club<>D.Club and B.Club<>C.Club and B.Club<>D.Club and C.Club<>D.Club and A.Player= :player 
-                order by A.A+B.A+C.A+D.A desc, A.A desc, B.A desc, C.A desc, D.A desc, p1, p2, p3, p4 limit 1;"""), {"player": player})
-        app.logger.info(f"Player received from request: {player}")
-        app.logger.info(f"Result: {result}")
-        # app.logger.info(f"Result: {result}")
-        for row in result:  
-            app.logger.info(f"Row: {row}")
-            ret = list(row)
-        cache.set(temp, ret, 3600)
+    if not result: 
+        # create a connection cursor
+        with engine.connect() as connection:
+            result = connection.execute(text("""
+                    select A.Player as p1,B.Player as p2, C.Player as p3,D.Player as p4 
+                    from assists A, assists B, assists C, assists D 
+                    where A.POS=B.POS and B.POS=C.POS and C.POS=D.POS and A.Club<>B.Club and A.club<>C.Club and A.Club<>C.Club and A.Club<>D.Club and B.Club<>C.Club and B.Club<>D.Club and C.Club<>D.Club and A.Player= :player 
+                    order by A.A+B.A+C.A+D.A desc, A.A desc, B.A desc, C.A desc, D.A desc, p1, p2, p3, p4 limit 1;"""), {"player": player})
+            app.logger.info(f"Player received from request: {player}")
+            app.logger.info(f"Result: {result}")
+            # app.logger.info(f"Result: {result}")
+            for row in result:  
+                app.logger.info(f"Row: {row}")
+                ret = list(row)
+            cache.set(temp, ret, 3600)
     resp = make_response({"players": ret}, 200)
     resp.headers["Content-Type"] = "application/json"
     resp.headers["X-CSE356"] = "66cfe2a89ba30e1a6c706759"
@@ -41,4 +41,4 @@ def hw6():
     return resp
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="80", debug=True)
+    app.run(host="0.0.0.0", port="80")
