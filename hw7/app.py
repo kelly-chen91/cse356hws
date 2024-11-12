ffc3e842451b0c21e397999be4c0650fc3aa22ef
Kelly Chen <kellychen966@gmail.com>
2024-11-11 21:20:04 -0500

    skull

--- hw7/app.py
+++ hw7/app.py
@@ -18,7 +18,7 @@ def hw6():
     ret = []
     temp = ''.join(player.split(' '))
     result = cache.get(temp)
-    app.logger.info(f'Cached Result: {result}')
+    # app.logger.info(f'Cached Result: {result}')
     if result:
         decodedRes = result.decode('utf-8')
         ret = ast.literal_eval(decodedRes)
@@ -30,12 +30,12 @@ def hw6():
                     from assists A, assists B, assists C, assists D 
                     where A.POS=B.POS and B.POS=C.POS and C.POS=D.POS and A.Club<>B.Club and A.club<>C.Club and A.Club<>C.Club and A.Club<>D.Club and B.Club<>C.Club and B.Club<>D.Club and C.Club<>D.Club and A.Player= :player 
                     order by A.A+B.A+C.A+D.A desc, A.A desc, B.A desc, C.A desc, D.A desc, p1, p2, p3, p4 limit 1;"""), {"player": player})
-            app.logger.info(f"Player received from request: {player}")
-            app.logger.info(f"Result: {result}")
+            # app.logger.info(f"Player received from request: {player}")
             # app.logger.info(f"Result: {result}")
-            for row in result:  
-                app.logger.info(f"Row: {row}")
-                ret = list(row)
+            # app.logger.info(f"Result: {result}")
+            # for row in result:  
+                # app.logger.info(f"Row: {row}")
+            ret = list(result[0])
             cache.set(temp, ret, 3600)
     resp = make_response({"players": ret}, 200)
     resp.headers["Content-Type"] = "application/json"
